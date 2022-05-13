e1bfca5d9c3e92e16063c4fa2e54a6b73641c1fd
kramstyles <mark.eke@decagon.dev>
2022-05-13 01:18:35 +0100

    added path in api/urls.py

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,1 +1,7 @@
-# urls
+from django.urls import path
+
+from .views.register_view import RegisterApiView
+
+urlpatterns = [
+    path('register/', RegisterApiView.as_view(), name='api-register'),
+]
--- project/api/views/register_view.py
+++ project/api/views/register_view.py
@@ -1,1 +1,9 @@
 from rest_framework import generics, response, status
+
+# from db.models.user_model import User
+from ...db.models.user_model import User
+from ..serializers import register_serializer
+
+
+class RegisterApiView(generics.CreateAPIView):
+    serializer_class = register_serializer
