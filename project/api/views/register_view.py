a3a0f00957bfd0bf5418f856cb5373f1c4fe39d4
kramstyles <mark.eke@decagon.dev>
2022-05-13 01:43:37 +0100

    created RegisterApiView

--- project/api/views/register_view.py
+++ project/api/views/register_view.py
@@ -1,9 +1,14 @@
-from rest_framework import generics, response, status
+from rest_framework import generics, response, status, permissions
 
-# from db.models.user_model import User
-from ...db.models.user_model import User
+# from ...db.models.user_model import User
+from db.models.user_model import User
 from ..serializers import register_serializer
 
 
 class RegisterApiView(generics.CreateAPIView):
-    serializer_class = register_serializer
+    serializer_class = register_serializer.RegisterSerializer
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny]
+
+
+
