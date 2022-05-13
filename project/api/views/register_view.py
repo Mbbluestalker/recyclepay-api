c56b5439b3b4aab79a893340d4dfa249a034cde9
kramstyles <mark.eke@decagon.dev>
2022-05-13 02:01:36 +0100

    updated RegisterApiView to save user

--- project/api/views/register_view.py
+++ project/api/views/register_view.py
@@ -1,14 +1,18 @@
-from rest_framework import generics, response, status, permissions
+from rest_framework import generics, response, status, exceptions
 
-# from ...db.models.user_model import User
-from db.models.user_model import User
 from ..serializers import register_serializer
 
 
 class RegisterApiView(generics.CreateAPIView):
     serializer_class = register_serializer.RegisterSerializer
     authentication_classes = []
-    permission_classes = [permissions.AllowAny]
+    permission_classes = []
 
-
-
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response({'message': 'Success',
+                                      'data': serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            raise exceptions.ValidationError(serializer.errors)
