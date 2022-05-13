250512f889a0bd38c5c9df424ea1683293f5fabc
kramstyles <mark.eke@decagon.dev>
2022-05-13 01:01:18 +0100

    updated register_serializer.py

--- project/api/serializers/register_serializer.py
+++ project/api/serializers/register_serializer.py
@@ -1,1 +1,21 @@
 from rest_framework import serializers
+
+from db.models.user_model import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
+
+        # Prevents the password from showing after submission
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        new_user = User.objects.create(**validated_data)
+        new_user.set_password(validated_data['password'])
+
+        new_user.save()
+        return new_user
