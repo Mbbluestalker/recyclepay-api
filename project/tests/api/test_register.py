8ceca0c393e0b654f9ac4fbbf683864555e699db
kramstyles <mark.eke@decagon.dev>
2022-05-14 01:20:34 +0100

    wrote test to check for incomplete data

--- project/tests/api/test_register.py
+++ project/tests/api/test_register.py
@@ -1,5 +1,5 @@
 from django.shortcuts import reverse
-from rest_framework import test, status
+from rest_framework import test, status, exceptions
 
 from db.models.user_model import User
 
@@ -10,6 +10,17 @@ class TestRegisterApi(test.APITestCase):
         response = self.client.post(reverse('api-register'))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_to_check_for_incomplete_data(self):
+        sample_data = {
+            'email': 'me@mail.com',
+            'password': 'pass',
+            'first_name': 'Micah',
+            'last_name': 'Tundra'
+        }
+        response = self.client.post(reverse('api-register'), sample_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'username': [exceptions.ErrorDetail(string='This field is required.', code='required')]})
+
     def test_to_determine_if_user_is_created_after_registration(self):
         sample_data = {
             'email': 'me@mail.com',
@@ -19,4 +30,12 @@ class TestRegisterApi(test.APITestCase):
             'last_name': 'Tundra'
         }
         initial_user_count = User.objects.all().count()
-        self.assertEqual(initial_user_count, 1)
+        response = self.client.post(reverse('api-register'), sample_data)
+
+        current_user_count = User.objects.all().count()
+        current_user = User.objects.get(username=sample_data['username'])
+
+        self.assertEqual(current_user_count, initial_user_count + 1)
+        self.assertIsNotNone(current_user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIsInstance(current_user, User)
