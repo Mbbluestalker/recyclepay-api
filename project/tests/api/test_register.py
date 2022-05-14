57c85b17e3c6f8ff723e846d1841effa9a6e1a1f
kramstyles <mark.eke@decagon.dev>
2022-05-14 01:27:19 +0100

    provided test cases

--- project/tests/api/test_register.py
+++ project/tests/api/test_register.py
@@ -5,7 +5,6 @@ from db.models.user_model import User
 
 
 class TestRegisterApi(test.APITestCase):
-
     def test_to_check_for_empty_parameters(self):
         response = self.client.post(reverse('api-register'))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
