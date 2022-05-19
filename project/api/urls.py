3690778dd92dd9d8ac050d287457cbefdc1d3988
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-05-19 17:36:40 +0100

    new amended password-reset code

--- project/api/urls.py
+++ project/api/urls.py
@@ -3,7 +3,6 @@ from api.views.reset_password_view import RequestPasswordResetEmail
 from django.urls import path
 
 
-
 urlpatterns = [
     path('auth/reset-password/<str:encoded_email>', RequestPasswordResetEmail.as_view(), name='reset-password'),
 ]
