269595681f55b3d016fbdb7c2395d782b8f9aa76
Rafihatu Bello <rafihatu.bello@barbora.lt>
2022-06-14 12:08:04 +0100

    update email settings

--- project/config/settings.py
+++ project/config/settings.py
@@ -159,6 +159,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="dummy@email.com")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="dummy123")
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
